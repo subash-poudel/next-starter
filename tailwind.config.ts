13902317b6dd109130c81a57f3389d61e85e992d
Subash Poudel <blogsubash@gmail.com>
2024-12-17 09:14:50 +0545

    main: Add sidebar component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
